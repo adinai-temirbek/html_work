efd2e3a06a3261398768c72fa43fe88eb69dc15b
Adinai Temirbekova <adinai.temirbekova_2028@ucentralasia.org>
2025-09-25 15:16:41 +0600

    refactored

---
+++ section3/assignment15/assignment15.js
@@ -0,0 +1,22 @@
+const cacheName = 'pwa-cache-v1';
+const assets = [
+  './',
+  './assignment15.html',
+  './assignment15.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
